58ad63ddc9ce50a30b352498fd59828a44b7dbcc
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-05 11:10:34 +0200

    added a file to test db connection

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+include('connection.php');
+
+$check = $mysqli->prepare('select subcategory_name from subcategories where id=1');
+$check->execute();
+$check->store_result();
+$num_rows = $check->num_rows();
+$check->bind_result($subcategory_name);
+$check->fetch();
+
+$response = array('name' => $subcategory_name);
+echo json_encode($response);
+?>
