254c3acbc34bda9c74677f21c1a6093febf8f175
Ayman Hajjar <aymanhajjar21@gmail.com>
2023-03-07 00:07:31 +0200

    fixed add_to_favorites

--- add_to_favorites.php
+++ add_to_favorites.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, OPTIONS");
 include('connection.php');
 $user_id = $_SESSION['user_id'];
 $product_id = $_POST['product_id'];
-$favorites = $mysqli->prepare('SELECT user_id, product_id FROM wishlists WHERE user_id=? AND product_id=?');
+$favorites = $mysqli->prepare('SELECT id FROM wishlists WHERE user_id=? AND product_id=?');
 $favorites->bind_param('ii', $user_id, $product_id);
 $favorites->execute();
 $favorites->store_result();
@@ -14,12 +14,12 @@ $favorites_exists = $favorites->num_rows();
 
 if ($favorites_exists > 0) {
     $delete = $mysqli->prepare('delete from wishlists where user_id=? and product_id=?');
-    $delete->bind_param('ss', $user_id, $product_id);
+    $delete->bind_param('ii', $user_id, $product_id);
     $delete->execute();
     echo json_encode(array("success" => true, "message" => "Product removed from wishlist"));
 } else {
     $insert = $mysqli->prepare('insert into wishlists (user_id, product_id) values (?, ?)');
-    $insert->bind_param('ss', $user_id, $product_id);
+    $insert->bind_param('ii', $user_id, $product_id);
     $insert->execute();
     echo json_encode(array("success" => true, "message" => "Product added to wishlist"));
 }
--- favorites.php
+++ favorites.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 include('connection.php');
-$user_id = $_GET['user_id'];
+$user_id = $_SESSION['user_id'];
 
-$favorites_page = $mysqli->prepare('select * from wishlists where user_id=?');
+$favorites_page = $mysqli->prepare('select p.id, p.product_name, p.price, c.image, p.discount from products p join wishlists w on p.id = w.product_id join product_colors c on p.id = c.product_id where w.user_id=? and p.main_color = c.color_id');
 $favorites_page->bind_param('i', $user_id);
 $favorites_page->execute();
 
