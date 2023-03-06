07ef55a320f6085d44361695cb45ca099d0a059c
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-03-06 02:16:51 +0200

    linked the table of users to the js file

--- display-users.php
+++ display-users.php
@@ -5,9 +5,22 @@ header('Access-Control-Allow-Origin: *');
  $sql="select * from users;";
  $result=mysqli_query($mysqli, $sql);
  $resultCheck=mysqli_num_rows($result);
- if($resultCheck>0){
+
+ if($resultCheck > 0) {
+    $response['status'] = 'exist';
     while($row=mysqli_fetch_assoc($result)){
         echo json_encode($row);
+        echo json_encode($response);
     }
+} else {
+    $response['status'] = 'empty';
+    echo json_encode($response);
  }
 ?>
+    <!-- $search_results = array(); -->
+
+    <!-- while ($row = $result->fetch_assoc()) {
+        $search_results[] = $row;
+    } -->
+
+    <!-- echo json_encode($search_results); -->
