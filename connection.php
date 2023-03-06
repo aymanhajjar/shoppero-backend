6fa104016ece0f508bf3b08c5be865a4e35b2395
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-03-06 23:55:12 +0200

    added the API of diaplaying users

--- connection.php
+++ connection.php
@@ -5,14 +5,4 @@ $host = "localhost";
 $db_user = "root";
 $db_pass = null;
 $db_name = "shoppero_db";
-$mysqli = new mysqli($host, $db_user, $db_pass, $db_name)
-?>
-<?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, OPTIONS");
-$host = "localhost";
-$db_user = "root";
-$db_pass = null;
-$db_name = "shoppero_db";
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
-?>
--- display-users.php
+++ display-users.php
@@ -2,7 +2,6 @@
 include('connection.php');
 header('Access-Control-Allow-Origin: *');
 
-
  $sql="select * from users;";
  $result=mysqli_query($mysqli, $sql);
  $resultCheck=mysqli_num_rows($result);
@@ -10,16 +9,12 @@ header('Access-Control-Allow-Origin: *');
  if($resultCheck > 0) {
     $response['status'] = 'exist';
     while($row=mysqli_fetch_assoc($result)){
-        echo json_encode($row);
+        $response[] = $row;
     }
+    echo json_encode($response);
 } else {
     $response['status'] = 'empty';
+    echo json_encode("$response");
  }
+
 ?>
-    <!-- $search_results = array(); -->
-
-    <!-- while ($row = $result->fetch_assoc()) {
-        $search_results[] = $row;
-    } -->
-
-    <!-- echo json_encode($search_results); -->
