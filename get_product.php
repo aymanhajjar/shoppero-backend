d18de9f1790d2c7b77c6945ecf1a6ba8db2622e0
MoeShoumar <mks38@mail.aub.edu>
2023-03-06 09:09:57 +0200

    get products and get product apis

--- get_product.php
+++ get_product.php
@@ -1,17 +1,17 @@
 <?php
-   header('Access-Control-Allow-Origin: *');
-   header("Access-Control-Allow-Methods: GET, OPTIONS");
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, OPTIONS");
     include('connection.php');
+    $id=$_GET['id'];
     
-    $products = $mysqli->prepare('select * from products');
-   
-    $products->execute();
+    $product = $mysqli->prepare('select * from products where id=?');
+    $product->bind_param('s', $id);
+    $product->execute();
 
-    $array = $products->get_result();
+    $array = $product->get_result();
     $response = []; 
     while($a = $array->fetch_assoc()){
         $response[] = $a;
     }
     echo json_encode($response);
-    
-    
+    ?>
--- get_products.php
+++ get_products.php
@@ -1,1 +1,16 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+ include('connection.php');
+ 
+ $products = $mysqli->prepare('select * from products');
+
+ $products->execute();
+
+ $array = $products->get_result();
+ $response = []; 
+ while($a = $array->fetch_assoc()){
+     $response[] = $a;
+ }
+ echo json_encode($response);
+ 
